8e5e1c203f77c24f76313d70b5355f3823e245d6
Romeo-04 <jatolentino@fit.edu.ph>
2025-11-09 17:02:18 +0800

    added prisma for database

--- smart-habit-tracker/app/layout.tsx
+++ smart-habit-tracker/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next';
+import "./globals.css";
+import type { Metadata } from "next";
+import Container from "./_components/Container";
 
-export const metadata : Metadata = {
-  title: 'Smart Habit Tracker',
-  description: 'Next 16 : React 19 : Tailwind v4'
-}
+export const metadata: Metadata = {
+  title: "Smart Habit Tracker",
+  description: "Next 16 : React 19 : Tailwind v4",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
-}
-) {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className="min-h-full">
+        <Container>{children}</Container>
       </body>
     </html>
-  )
+  );
 }
